15593325c3fa504b241950679c5ba12c858ea3f8
Kensuke24 <hibis.tori@gmail.com>
2019-03-21 12:06:17 +0900

    欠損値の表示

--- Titanic_Machine Learning from Disaster/main.py
+++ Titanic_Machine Learning from Disaster/main.py
@@ -1,6 +1,24 @@
-Python
 import pandas as pd
 import numpy as np
 
-train = pd.read_csv("../ディレクトリを指定/train.csv")
-test = pd.read_csv("../ディレクトリを指定/test.csv")
+
+train = pd.read_csv("data/train.csv")   # train.csvの読み込み
+test = pd.read_csv("data/test.csv")   # test.csvの読み込み
+'''
+print(train.shape)  # train.csvの行、列数を表示
+
+print(test.describe())  # 基本統計量の確認。
+'''
+
+
+def kesson_table(df):   # dataframe（train.csvとtest.csv）の欠損データをisnull()で探してカラムごとに返す関数
+    null_val = df.isnull().sum()    # カラムごとの欠損数
+    percent = 100 * df.isnull().sum()/len(df)   #
+    kesson_table = pd.concat([null_val, percent], axis=1)
+    kesson_table_ren_columns = kesson_table.rename(
+    columns = {0 : '欠損数', 1 : '%'})
+    return kesson_table_ren_columns
+    kesson_table(train)
+    kesson_table(test)
+
+print(kesson_table(train))
