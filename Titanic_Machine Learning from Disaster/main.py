016f640d26a78a25507b5d648e2b504daba2835d
Kensuke24 <hibis.tori@gmail.com>
2019-03-21 13:39:13 +0900

    文字列の置換

--- Titanic_Machine Learning from Disaster/main.py
+++ Titanic_Machine Learning from Disaster/main.py
@@ -13,12 +13,21 @@ print(test.describe())  # 基本統計量の確認。
 
 def kesson_table(df):   # dataframe（train.csvとtest.csv）の欠損データをisnull()で探してカラムごとに返す関数
     null_val = df.isnull().sum()    # カラムごとの欠損数
-    percent = 100 * df.isnull().sum()/len(df)   #
+    percent = 100 * df.isnull().sum()/len(df)   # 全体に対する欠損数の割合
     kesson_table = pd.concat([null_val, percent], axis=1)
-    kesson_table_ren_columns = kesson_table.rename(
-    columns = {0 : '欠損数', 1 : '%'})
+    kesson_table_ren_columns = kesson_table.rename(columns = {0 : '欠損数', 1 : '%'})
     return kesson_table_ren_columns
     kesson_table(train)
     kesson_table(test)
 
-print(kesson_table(train))
+
+train["Age"] = train["Age"].fillna(train["Age"].median())   # Ageカラムの欠損値にAgeの中央値を入れる
+train["Embarked"] = train["Embarked"].fillna("S")   # Embarkedカラムの欠損値にSを入れる
+kesson_table(train)
+
+train["Sex"][train["Sex"] == "male"] = 0    # Sexカラムの文字列maleを0に置き換える
+train["Sex"][train["Sex"] == "female"] = 1    # Sexカラムの文字列femaleを1に置き換える
+train["Embarked"][train["Embarked"] == "S" ] = 0    # Embarkedカラムの文字列Sを0に置き換える
+train["Embarked"][train["Embarked"] == "C" ] = 1    # Embarkedカラムの文字列Cを1に置き換える
+train["Embarked"][train["Embarked"] == "Q"] = 2    # Embarkedカラムの文字列Qを2に置き換える
+train.head(10)
