91f0e8e595b7b05a9da7e0f6ac6e7e47a022d5e3
Kensuke24 <hibis.tori@gmail.com>
2019-03-21 21:58:16 +0900

    予測モデルの作成

--- Titanic_Machine Learning from Disaster/main.py
+++ Titanic_Machine Learning from Disaster/main.py
@@ -1,5 +1,10 @@
+'''
+https://www.codexa.net/kaggle-titanic-beginner/を参考に勉強させていただきました。
+'''
+
 import pandas as pd
 import numpy as np
+from sklearn import tree
 
 
 train = pd.read_csv("data/train.csv")   # train.csvの読み込み
@@ -31,3 +36,22 @@ train["Embarked"][train["Embarked"] == "S" ] = 0    # Embarkedカラムの文字
 train["Embarked"][train["Embarked"] == "C" ] = 1    # Embarkedカラムの文字列Cを1に置き換える
 train["Embarked"][train["Embarked"] == "Q"] = 2    # Embarkedカラムの文字列Qを2に置き換える
 
+test["Age"] = test["Age"].fillna(test["Age"].median())
+test["Sex"][test["Sex"] == "male"] = 0
+test["Sex"][test["Sex"] == "female"] = 1
+test["Embarked"][test["Embarked"] == "S"] = 0
+test["Embarked"][test["Embarked"] == "C"] = 1
+test["Embarked"][test["Embarked"] == "Q"] = 2
+test.Fare[152] = test.Fare.median()   # Fareカラムの欠損値(152)にFareの中央値を入れる
+
+
+# 「train」の目的変数と説明変数の値を取得
+target = train["Survived"].values
+features_one = train[["Pclass", "Sex", "Age", "Fare"]].values
+# 決定木の作成
+my_tree_one = tree.DecisionTreeClassifier()
+my_tree_one = my_tree_one.fit(features_one, target)
+# 「test」の説明変数の値を取得
+test_features = test[["Pclass", "Sex", "Age", "Fare"]].values
+# 「test」の説明変数を使って「my_tree_one」のモデルで予測
+my_prediction = my_tree_one.predict(test_features)
